f6676930ebe5009a732bd0f72b498f0a95530217
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 11:17:04 +0700

    Update dom_var.js

--- dom_var.js
+++ dom_var.js
@@ -6,6 +6,9 @@
 //   "Traffic",
 //   "Pagelike",
 // ];
+const accessToken =
+  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
+
 let isBrand = true;
 const accName = "Ampersand";
 const accAvatar = "DOM-img/ampersand.jpg";
