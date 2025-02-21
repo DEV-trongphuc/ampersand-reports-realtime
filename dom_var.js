0658873cdf03d428a8de18d7d8dde8cac4ab9a51
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 10:57:16 +0700

    Create dom_var.js

---
+++ dom_var.js
@@ -0,0 +1,21 @@
+// const quickFilter = [
+//   "Lead Form",
+//   "Awareness",
+//   "Engagement",
+//   "Message",
+//   "Traffic",
+//   "Pagelike",
+// ];
+let isBrand = true;
+const accName = "Ampersand";
+const accAvatar = "DOM-img/ampersand.jpg";
+const adAccountId = "676599667843841"; // PHÚC LỘC THỌ
+const quickFilter = [
+  "Haagen Dazs",
+  "Snowee",
+  "Esta",
+  "TRB",
+  "BEAN",
+  "Le Petit",
+];
+
