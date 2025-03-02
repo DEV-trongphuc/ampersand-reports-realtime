49d67edbcd047e31b5e26b29badb5c16b32c79d7
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-03-02 17:14:18 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,24 +1,12 @@
-// const quickFilter = [
-//   "Lead Form",
-//   "Awareness",
-//   "Engagement",
-//   "Message",
-//   "Traffic",
-//   "Pagelike",
-// ];
-const accessToken =
-  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
-
-let isBrand = true;
-const accName = "Ampersand";
-const accAvatar = "DOM-img/ampersand.jpg";
-const adAccountId = "676599667843841"; // PHÚC LỘC THỌ
-const quickFilter = [
-  "Haagen Dazs",
-  "Snowee",
-  "Esta",
-  "TRB",
-  "BEAN",
-  "Le Petit",
+let listAccount = [
+ 
+  {
+    brand: true,
+    quick: ["Haagen Dazs", "Snowee", "Esta", "TRB", "BEAN", "Le Petit"],
+    avatar: "https://ampersand-reports-dom.netlify.app/DOM-img/ampersand.jpg",
+    name: "Ampersand",
+    id: "676599667843841",
+    access:
+      "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM",
+  },
 ];
-
