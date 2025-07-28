b189713b6cf2a9f1202a1bb6d19a650fd720d01c
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-07-28 11:42:48 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -7,6 +7,6 @@ let listAccount = [
     name: "Ampersand",
     id: "676599667843841",
     access:
-      "EAAOahG2GiWkBOZBCDqccffl67c74Tmpjnha7ClZBvxnO9VKcLM4qnH422I9PhE25sOmROOs3hjynmu4tUSBdL6V5BZB1ET3IHAijOtwZAsJpuhZCSe7Vw2ZCA3qJo75EimqmBmThSczRgxN3cBkPafItCsiZA17MXd8ZBDjRGNfjRO8cKspFPtMJUaZCjkkUfO2Uzxrccwoia",
+      "EAAOahG2GiWkBPGVhHhxkQTXsgIkiqVEmdRjABTuQVDHTDshQZBvchHuSsZCl0V3gWQpiSMb7qYQg1mdliCw8tdUICJEYrCqOaFrN4MbZCseCXzOGy9r07HDAo1rUs4q0yqerZBwln4iQlaTlubVZA2llHZBjmYRxYlfWBhLUOjlGrFxacZC8ZC5jvZAkBISrM7QArSKHNLt7q",
   },
 ];
